33e554eca0a1a5df2db473d1239aeb83c1f66a1e
hxnnahshink <shinkhan@msu.edu>
2019-04-04 13:05:50 -0400

    final commit

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
-function onReady() {
-  const toDos = [];
+function onReady () {
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-   function createNewToDo() {
-         const newToDoText = document.getElementById('newToDoText');
-          if (!newToDoText.value) { return; }
-         toDos.push({
-     title: newToDoText.value,
-     complete: false
-   });
-   function renderTheUI() {
-     const toDoList = document.getElementById('toDoList');
-     toDoList.textContent = '';
-     toDos.forEach(function(toDo) {
-       const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
-       newLi.textContent = toDo.title;
-       toDoList.appendChild(newLi);
-   newLi.appendChild(checkbox);
+  let id = 0;
 
+  function createNewToDo () {
+    const newToDoText = document.getElementById('newToDoText');
+    if(!newToDoText.value){ return; }
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    id++;
+
+    newToDoText.value = '';
+    renderTheUI();
+  }
+
+  function deleteToDo(id) {
+    return toDos.filter(toDo => toDo.id != id);
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+      toDos.forEach(function(toDo) {
+        const newLi = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete";
+
+        newLi.textContent = toDo.title;
+
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+        newLi.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener('click', () => {
+          toDos = deleteToDo(toDo.id);
+          renderTheUI();
+          saveToDos();
+    });
   });
-
- }
-   addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-  createNewToDo();
-});
-  renderTheUI();
-   newToDoText.value = '';
-     renderTheUI();
-
 }
-window.onload = function(){
-  onReady();
 
- };
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+    renderTheUI();
+  });
+  renderTheUI();
+}
+
+window.onload = function() {
+  onReady();
+};
